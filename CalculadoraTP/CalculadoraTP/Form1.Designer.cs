bb88fb8dbe112926b7acdb1e9503325b9acdb79a
Kleber Brito <kleber.brito@etec.sp.gov.br>
2019-09-27 22:16:37 -0300

    [feat] Adicionado calculadora

---
+++ CalculadoraTP/CalculadoraTP/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace CalculadoraTP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnSoma = new System.Windows.Forms.Button();
+            this.btnSubtrai = new System.Windows.Forms.Button();
+            this.btnMultiplica = new System.Windows.Forms.Button();
+            this.btnDividi = new System.Windows.Forms.Button();
+            this.btnRaiz = new System.Windows.Forms.Button();
+            this.btnEleva = new System.Windows.Forms.Button();
+            this.btnConcatena = new System.Windows.Forms.Button();
+            this.btnLimpa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(222, 25);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(128, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.Location = new System.Drawing.Point(57, 77);
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.Size = new System.Drawing.Size(76, 21);
+            this.btnSoma.TabIndex = 2;
+            this.btnSoma.Text = "+";
+            this.btnSoma.UseVisualStyleBackColor = true;
+            this.btnSoma.Click += new System.EventHandler(this.btnSoma_Click);
+            // 
+            // btnSubtrai
+            // 
+            this.btnSubtrai.Location = new System.Drawing.Point(139, 77);
+            this.btnSubtrai.Name = "btnSubtrai";
+            this.btnSubtrai.Size = new System.Drawing.Size(76, 24);
+            this.btnSubtrai.TabIndex = 3;
+            this.btnSubtrai.Text = "-";
+            this.btnSubtrai.UseVisualStyleBackColor = true;
+            this.btnSubtrai.Click += new System.EventHandler(this.btnSubtrai_Click);
+            // 
+            // btnMultiplica
+            // 
+            this.btnMultiplica.Location = new System.Drawing.Point(221, 77);
+            this.btnMultiplica.Name = "btnMultiplica";
+            this.btnMultiplica.Size = new System.Drawing.Size(76, 24);
+            this.btnMultiplica.TabIndex = 4;
+            this.btnMultiplica.Text = "x";
+            this.btnMultiplica.UseVisualStyleBackColor = true;
+            this.btnMultiplica.Click += new System.EventHandler(this.btnMultiplica_Click);
+            // 
+            // btnDividi
+            // 
+            this.btnDividi.Location = new System.Drawing.Point(57, 113);
+            this.btnDividi.Name = "btnDividi";
+            this.btnDividi.Size = new System.Drawing.Size(76, 24);
+            this.btnDividi.TabIndex = 5;
+            this.btnDividi.Text = "/";
+            this.btnDividi.UseVisualStyleBackColor = true;
+            this.btnDividi.Click += new System.EventHandler(this.btnDividi_Click);
+            // 
+            // btnRaiz
+            // 
+            this.btnRaiz.Location = new System.Drawing.Point(139, 113);
+            this.btnRaiz.Name = "btnRaiz";
+            this.btnRaiz.Size = new System.Drawing.Size(76, 24);
+            this.btnRaiz.TabIndex = 6;
+            this.btnRaiz.Text = "RAIZ";
+            this.btnRaiz.UseVisualStyleBackColor = true;
+            this.btnRaiz.Click += new System.EventHandler(this.btnRaiz_Click);
+            // 
+            // btnEleva
+            // 
+            this.btnEleva.Location = new System.Drawing.Point(222, 113);
+            this.btnEleva.Name = "btnEleva";
+            this.btnEleva.Size = new System.Drawing.Size(76, 24);
+            this.btnEleva.TabIndex = 7;
+            this.btnEleva.Text = "X ELEVA Y";
+            this.btnEleva.UseVisualStyleBackColor = true;
+            this.btnEleva.Click += new System.EventHandler(this.btnEleva_Click);
+            // 
+            // btnConcatena
+            // 
+            this.btnConcatena.Location = new System.Drawing.Point(57, 153);
+            this.btnConcatena.Name = "btnConcatena";
+            this.btnConcatena.Size = new System.Drawing.Size(158, 24);
+            this.btnConcatena.TabIndex = 8;
+            this.btnConcatena.Text = "CONCATENA";
+            this.btnConcatena.UseVisualStyleBackColor = true;
+            this.btnConcatena.Click += new System.EventHandler(this.btnConcatena_Click);
+            // 
+            // btnLimpa
+            // 
+            this.btnLimpa.Location = new System.Drawing.Point(221, 153);
+            this.btnLimpa.Name = "btnLimpa";
+            this.btnLimpa.Size = new System.Drawing.Size(76, 24);
+            this.btnLimpa.TabIndex = 9;
+            this.btnLimpa.Text = "LIMPAR";
+            this.btnLimpa.UseVisualStyleBackColor = true;
+            this.btnLimpa.Click += new System.EventHandler(this.btnLimpa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 268);
+            this.Controls.Add(this.btnLimpa);
+            this.Controls.Add(this.btnConcatena);
+            this.Controls.Add(this.btnEleva);
+            this.Controls.Add(this.btnRaiz);
+            this.Controls.Add(this.btnDividi);
+            this.Controls.Add(this.btnMultiplica);
+            this.Controls.Add(this.btnSubtrai);
+            this.Controls.Add(this.btnSoma);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnSoma;
+        private System.Windows.Forms.Button btnSubtrai;
+        private System.Windows.Forms.Button btnMultiplica;
+        private System.Windows.Forms.Button btnDividi;
+        private System.Windows.Forms.Button btnRaiz;
+        private System.Windows.Forms.Button btnEleva;
+        private System.Windows.Forms.Button btnConcatena;
+        private System.Windows.Forms.Button btnLimpa;
+    }
+}
+
---
+++ CalculadoraTP/CalculadoraTP/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraTP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConcatena_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text + textBox2.Text);
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+            int n1;
+            int n2;
+            int soma;
+            // usa o convert para converter para o n1 receber a string textBox1
+            n1 = Convert.ToInt16(textBox1.Text);
+            n2 = Convert.ToInt16(textBox2.Text);
+            soma = n1 + n2;
+            MessageBox.Show(Convert.ToString(soma));
+                               
+        }
+
+        private void btnSubtrai_Click(object sender, EventArgs e)
+        {
+            int n1;
+            int n2;
+            int subtrai;
+            n1 = Convert.ToInt16(textBox1.Text);
+            n2 = Convert.ToInt16(textBox2.Text);
+            subtrai = n1 - n2;
+            MessageBox.Show(Convert.ToString(subtrai));
+
+        }
+
+        private void btnMultiplica_Click(object sender, EventArgs e)
+        {
+            int n1;
+            int n2;
+            int multiplica;
+            n1 = Convert.ToInt16(textBox1.Text);
+            n2 = Convert.ToInt16(textBox2.Text);
+            multiplica = n1 * n2;
+            MessageBox.Show(Convert.ToString(multiplica));
+        }
+
+        private void btnDividi_Click(object sender, EventArgs e)
+        {
+            int n1;
+            int n2;
+            int dividi;
+            n1 = Convert.ToInt16(textBox1.Text);
+            n2 = Convert.ToInt16(textBox2.Text);
+            dividi = n1 / n2;
+            MessageBox.Show(Convert.ToString(dividi));
+        }
+
+        private void btnEleva_Click(object sender, EventArgs e)
+        {
+            int n1;
+            int n2;
+            int eleva;
+            n1 = Convert.ToInt32(textBox1.Text);
+            n2 = Convert.ToInt32(textBox2.Text);
+            // usa-se (Math.Pow(n1, n2)) para elevar uma variável a outra.
+            eleva = Convert.ToInt32(Math.Pow(n1, n2));
+            //precisa converter para string, pois ele está preparado para receber um número inteiro
+            
+            MessageBox.Show(Convert.ToString(eleva));
+        }
+
+        private void btnRaiz_Click(object sender, EventArgs e)
+        {
+            double n1;
+            double resultado;
+            n1 = Convert.ToDouble(textBox1.Text);
+            resultado = Math.Sqrt(n1);
+
+            MessageBox.Show(Convert.ToString(resultado));            
+            
+        }
+
+        private void btnLimpa_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+        }
+    }
+}
